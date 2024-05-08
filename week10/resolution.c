3d09643d4c426b65a944bc10e79d730612b66ca2
jaehun00 <brian001117@gmaail.com>
2024-05-08 17:39:33 +0900

    change

---
+++ week10/resolution.c
@@ -0,0 +1,94 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+
+void read_file(float *, int, int, char *);
+void median_filtering(void *, int, int, int(*fcmp)(const void *, const void *));
+void noise_remove(float *, float *, int, int);
+
+int intcmp(const void *a, const void *b){
+    return (*(int *)a - *(int *)b);
+}
+
+void main(){
+    int height = 44, width = 45;
+    char filename[100] = "noisy_data.txt";
+
+    float* input = (float*)calloc(height*width, sizeof(float));
+    float* output = (float*)calloc(height*width, sizeof(float));
+
+    read_file(input, height, width, filename);
+    noise_remove(input, output, height, width);
+
+    free(input);
+    free(output);
+}
+
+void noise_remove(float *input, float *output, int height, int width){
+    int x, y, xx, yy;
+    FILE *fp;
+    char filename[100] = "result.txt";
+    int cnt;    
+    int len = 2;
+    // 5X5
+    float *temp = (float*)calloc((2*len+1)*(2*len+1), sizeof(float));
+
+    for(y=0; y<height; y++){
+        for(x=0; x<width; x++){
+
+            cnt = 0;
+            for(yy=y-len; yy<=y+len; yy++){
+                for(xx=x-len; xx<=x+len; xx++){
+                    temp[cnt] = input[yy*(2*len+1)+xx];
+                    cnt++;
+                }
+            }
+            median_filtering(temp, cnt, sizeof(float), intcmp);
+            output[y*width + x] = temp[cnt%2+1];
+        }
+    }
+
+    fp = fopen(filename, "w");
+
+    for(y=0; y<height; y++){
+        for(x=0; x<width; x++){
+            fprintf(fp, "%.2f", &output[y*width+x]);
+            fprintf(fp, " ");
+        }
+        fprintf(fp, "\n");
+    }
+    fclose(fp);
+}
+
+void median_filtering(void *base, int nelem, int width, int(*fcmp)(const void *, const void *)){
+    void *t;
+    int i, j;
+
+    t = malloc(width);
+    for(i=0; i < nelem; i++){
+        j = i;
+        memcpy(t, (char *)base + i*width, width);
+        while(j>0 && fcmp((char *)base + (j-1)*width, t) > 0){
+            memcpy((char *)base + j*width, (char *)base + (j-1)*width, width);
+            j--;
+        }
+        memcpy((char *)base + j*width, t, width);
+    }
+    free(t);
+}
+
+void read_file(float *input, int height, int width, char filename[])
+{
+    int x, y;
+    FILE *fp;
+    
+    fp = fopen(filename, "r");
+
+    for(y=0; y<height; y++){
+        for(x=0; x<width; x++){
+            fscanf(fp, "%f", &input[y*width+x]);
+        }
+        fscanf(fp, "\n");
+    }
+    fclose(fp);
+}
--- week9/exam9_2.c
+++ week9/exam9_2.c
@@ -3,6 +3,12 @@
 # include <string.h>
 # include <time.h>
 
+#define SIZE 4
+
+int intcmp(const void *a, const void *b){
+    return(*(int *)a - *(int *)b);
+}
+
 void insert_sort(int *a, int N){
     int i, j, t;
     for(i=1; i<N; i++){
@@ -14,6 +20,23 @@ void insert_sort(int *a, int N){
         }
         a[j] = t;
     }
+}
+
+void gen_insert_sort(void *base, int nelem, int width, int(*fcmp)(const void *, const void *)){
+    void *t;
+    int i, j;
+
+    t = malloc(width);
+    for(i=0; i < nelem; i++){
+        j = i;
+        memcpy(t, (char *)base + i*width, width);
+        while(j>0 && fcmp((char *)base + (j-1)*width, t) > 0){
+            memcpy((char *)base + j*width, (char *)base + (j-1)*width, width);
+            j--;
+        }
+        memcpy((char *)base + j*width, t, width);
+    }
+    free(t);
 }
 
 void indirect_insert_sort(int *a, int *index, int N){
@@ -28,7 +51,7 @@ void indirect_insert_sort(int *a, int *index, int N){
             index[j] = index[j-1];
             j--;
         }
-        index[j] = i;
+        a[index[j]] = a[t];
     }
 }
 
@@ -48,22 +71,56 @@ void shell_sort(int *a, int N){
         }
     }
 }
+
+void gen_shell_sort(void *base, int nelem, int width, int(*fcmp)(const void *, const void *)){
+    int i, j, k, h;
+    void *v;
+    v = malloc(width);
+
+    for(h=nelem/2; h>0; h/=2){
+        for(i=0; i<h; i++){
+            for(j=i+h; j<nelem; j+=h){
+                memcpy(v, (char *)base + j*width, width);
+                k = j;
+                while(k>h-1 && fcmp((char *)base + (k-h)*width, v) > 0){
+                    memcpy((char *)base + k*width, (char *)base + (k-h)*width, width);
+                    k -= h;
+                }
+                memcpy((char *)base + k*width, v, width);
+            }
+        }
+    }
+    free(v);
+}
 void main(){
-    int N = 4;
     int i;
     
-    int a[4] = {1,5,6,3};
-    int index[4];
-    indirect_insert_sort(a,index,N);
-    for(i=0;i<N;i++){
+    int a[SIZE] = {1,5,6,3};
+    int index[SIZE];
+    indirect_insert_sort(a,index,SIZE);
+    for(i=0;i<SIZE;i++){
         printf("%d", a[index[i]]);
     }
     printf("\n");
 
-    int b[4] = {1,5,6,3};
-    shell_sort(b,N);
-    for(i=0;i<N;i++){
+    int b[SIZE] = {1,5,6,3};
+    shell_sort(b,SIZE);
+    for(i=0;i<SIZE;i++){
         printf("%d", b[i]);
+    }
+    printf("\n");
+
+    int c[SIZE] = {1,5,6,3};
+    gen_insert_sort(c, SIZE, sizeof(int), intcmp);
+    for(i=0; i<SIZE; i++){
+        printf("%d", c[i]);
+    }
+    printf("\n");
+
+    int d[SIZE] = {1,5,6,3};
+    gen_shell_sort(d, SIZE, sizeof(int), intcmp);
+    for(i=0; i<SIZE; i++){
+        printf("%d", d[i]);
     }
     printf("\n");
 }
